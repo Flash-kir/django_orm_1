29279f9be13a14633712ddf004d0d9538a177a65
Alexey <flash-kir@yandex.ru>
2022-09-17 22:21:05 +0300

    1-5 steps

--- security_panel/manage.py
+++ security_panel/manage.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "security_panel.settings")
+
 import sys
+import django
+django.setup()
+
+from datacenter.models import Passcard
+
+
+def print_passcard(passcard):
+    print(f'owner_name: {passcard.owner_name}')
+    print(f'passcode: {passcard.passcode}')
+    print(f'created_at: {passcard.created_at}')
+    print(f'is_active: {passcard.is_active}')
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'security_panel.settings')
+    passcards = Passcard.objects.all()
+    active_passcards = Passcard.objects.filter(is_active=True)
+    print(f'all passcards: {len(passcards)}')
+    print(f'active passcards: {len(active_passcards)}')
+    print_passcard(passcards[0])
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
